fe2e66a8029b8b63c14324e64f37f75f84180643
ikxneng <97749494+ikxneng@users.noreply.github.com>
2024-07-18 13:29:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const addButton = document.getElementById('add-task-btn');
+    const taskInput = document.getElementById('task-input');
+    const taskList = document.getElementById('task-list');
+
+    function addTask(){
+        const taskText = taskInput.value.trim();
+
+        //Task Creation and Removal    
+        if(taskText === ""){
+            alert('Enter a Task');
+        }else {
+            const taskItem = document.createElement('li');
+            taskItem.textContent = taskText;
+
+            const removeButton = document.createElement('button');
+            removeButton.textContent = "Remove";
+            removeButton.className = 'remove-btn';
+
+            removeButton.addEventListener('click', function(){
+                taskList.removeChild(taskItem);
+        });
+
+        // Append the remove button to the task item
+        taskItem.appendChild(removeButton);
+
+        // Append the task item to the task list
+        taskList.appendChild(taskItem);
+
+
+        // Clear the input field
+        taskInput.value = "";
+            
+        }
+    }
+
+    addButton.addEventListener('click', addTask);
+
+    // Add task when Enter key is pressed
+    taskInput.addEventListener('keypress', function(event){
+        if (event.key === 'Enter'){
+            addTask();
+        }
+    });
+
+
+});
+
+document.addEventListener('DOMContentLoaded', addTask);
+
 
